9818688aa1e4d683807fbb2d465624d7af634298
simidawalang <simidawalang@gmail.com>
2022-02-04 08:20:20 +0100

    push to repo

---
+++ data-structures/linkedLists.js
@@ -0,0 +1,23 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length = 1;
+    }
+}
+
+const testLinkedList = new LinkedList(6);
+console.log(testLinkedList);

d635f1d3353dc909a4cc0feaf7ec26a68b056b88
simidawalang <simidawalang@gmail.com>
2022-02-06 11:48:38 +0100

    added pop method

--- data-structures/linkedLists.js
+++ data-structures/linkedLists.js
@@ -10,8 +10,9 @@ class LinkedList {
         const newNode = new Node(value);
         this.head = newNode;
         this.tail = newNode;
-        this.length = 1; 
+        this.length = 1;
     }
+
     push(value) {
         const newNode = new Node(value);
         if (!this.head) {
@@ -24,8 +25,27 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    pop() {
+        if (!this.head) return undefined;
+        let temp = this.head;
+        let pre = this.head;
+        while(temp.next) {
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+    }
 }
 
-let myLinkedList = new LinkedList(6);
-myLinkedList.push(4);
-console.log(myLinkedList);
+let myLinkedList = new LinkedList(4);
+myLinkedList.pop();
+myLinkedList.pop();
+console.log(myLinkedList)

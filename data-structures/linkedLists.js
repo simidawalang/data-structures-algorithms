16da8cd0357dcd1c38db77e35d88d2b4bd3bae99
simidawalang <simidawalang@gmail.com>
2022-02-06 15:00:13 +0100

    added get()

--- data-structures/linkedLists.js
+++ data-structures/linkedLists.js
@@ -26,23 +26,6 @@ class LinkedList {
     return this;
   }
 
-  // pop() {
-  //     if (!this.head) return undefined;
-  //     let temp = this.head;
-  //     let pre = this.head;
-  //     while(temp.next) {
-  //         pre = temp;
-  //         temp = temp.next;
-  //     }
-  //     this.tail = pre;
-  //     this.tail.next = null;
-  //     this.length--;
-  //     if(this.length === 0) {
-  //         this.head = null;
-  //         this.tail = null;
-  //     }
-  //     return temp;
-  // }
   pop() {
     if (!this.head) return undefined;
 
@@ -76,9 +59,30 @@ class LinkedList {
     this.length++;
     return this;
   }
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if(this.length === 0) {
+        this.tail = null;
+    }
+    return temp;
+  }
+
+  get(index) {
+      if(index < 0 || index >= this.length) return undefined;
+      let temp = this.head;
+      for(let i = 0; i < index; i++) {
+          temp = temp.next;
+      }
+      return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(4);
 myLinkedList.push(7);
 myLinkedList.unshift(9);
-console.log(myLinkedList);
+myLinkedList.shift();
+console.log(myLinkedList.get(0));

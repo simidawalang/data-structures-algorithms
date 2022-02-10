f2a67e9d0daa049f4986ee168d4c36713365a6ec
simidawalang <simidawalang@gmail.com>
2022-02-10 14:46:53 +0100

    added get, set, insert, remove, reverse

--- data-structures/linkedLists.js
+++ data-structures/linkedLists.js
@@ -18,17 +18,16 @@ class LinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
+      this.length = 1;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
     }
     this.length++;
-    return this;
   }
 
   pop() {
     if (!this.head) return undefined;
-
     let temp = this.head;
     let pre = this.head;
 
@@ -39,12 +38,10 @@ class LinkedList {
     this.tail = pre;
     this.tail.next = null;
     this.length--;
-
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
-    return temp;
   }
   unshift(value) {
     const newNode = new Node(value);
@@ -57,32 +54,80 @@ class LinkedList {
       this.head = newNode;
     }
     this.length++;
-    return this;
   }
+
   shift() {
     if (!this.head) return undefined;
     let temp = this.head;
     this.head = this.head.next;
     temp.next = null;
     this.length--;
-    if(this.length === 0) {
-        this.tail = null;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
     }
     return temp;
   }
 
-  get(index) {
-      if(index < 0 || index >= this.length) return undefined;
-      let temp = this.head;
-      for(let i = 0; i < index; i++) {
-          temp = temp.next;
-      }
-      return temp;
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+
+    const newNode = new Node(value);
+    let temp = this.get(index - 1);
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    let before = this.get(index - 1);
+    let temp = before.next;
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp;
+    let prev = null;
+    for (let i = 0; i < index; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
   }
 }
 
-let myLinkedList = new LinkedList(4);
-myLinkedList.push(7);
-myLinkedList.unshift(9);
-myLinkedList.shift();
-console.log(myLinkedList.get(0));
+const myLinkedList = new LinkedList(1);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.remove(2);
+console.log(myLinkedList);
